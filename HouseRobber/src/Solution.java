4c9efcd6db6fe3fc1f6280ead3f7d7a33f7f3d8f
Mengqi Pei <mengqipei@gmail.com>
2015-06-12 23:45:26 +0800

    HouseRobber 动态规划 280ms（递归算法会超时，改为迭代）

---
+++ HouseRobber/src/Solution.java
@@ -0,0 +1,52 @@
+/**
+ * Created by Mengqi on 15/6/12.
+ * 280 ms
+ */
+public class Solution {
+    //   迭代方法
+    public int rob(int nums[]) {
+        int length = nums.length;
+        if(length == 0) return 0;
+        else if(length == 1) return nums[0];
+        else if(length == 2) return Math.max(nums[0], nums[1]);
+
+        int M_minus2 = nums[0], //M[0]
+            M_minus1 = Math.max(nums[0], nums[1]),//M[1]
+            M = 0;          //M[i]
+        for(int i = 2; i < length; i++, M_minus2 = M_minus1, M_minus1 = M) {
+            M = Math.max(M_minus2+nums[i], M_minus1);
+        }
+        return M;
+    }
+
+
+//    递归方法
+//    报 TimeOut(不知道为什么)
+//    public int rob(int nums[]) {
+//        int length = nums.length;
+//        if(length == 0) return 0;
+//        else if(length == 1) return nums[0];
+//        else if(length == 2) return Math.max(nums[0] >= nums[1]);
+//
+//        int MoneyWithLastHouse = rob(nums,length-2)+nums[length-1];
+//        int MoneyWithoutLastHouse = rob(nums,length-1);
+//        return Math.max(MoneyWithLastHouse, MoneyWithoutLastHouse);
+//    }
+//
+//    private int rob(int nums[], int length) {
+//        if(length == 0) return 0;
+//        else if(length == 1) return nums[0];
+//        else if(length == 2) return Math.max(nums[0] >= nums[1]);
+//
+//        int MoneyWithLastHouse = rob(nums,length-2)+nums[length-1];
+//        int MoneyWithoutLastHouse = rob(nums,length-1);
+//        return Math.max(MoneyWithLastHouse, MoneyWithoutLastHouse);
+//    }
+
+    public static void main(String[] args) {
+        int nums[] = {3, 6, 2, 3, 4};
+//        int nums[] = {114,117,207,117,235,82,90,67,143,146,53,108,200,91,80,223,58,170,110,236,81,90,222,160, 165, 195, 187, 199, 114,235,197,187,69,129,64,214,228,78,188,67,205,94,205,169,241,202,144,240};
+        Solution solution = new Solution();
+        System.out.println(solution.rob(nums));
+    }
+}
